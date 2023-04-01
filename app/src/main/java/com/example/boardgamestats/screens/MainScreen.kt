bd7c4d4fd09e832bc4bf93f82c337da6a602fe00
Adrian Kokot <kokotadriann@gmail.com>
2023-04-01 14:38:09 +0200

    Add board game search results icons animation

--- app/src/main/java/com/example/boardgamestats/screens/MainScreen.kt
+++ app/src/main/java/com/example/boardgamestats/screens/MainScreen.kt
@@ -1,17 +1,20 @@
 package com.example.boardgamestats.screens
 
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.navigation.compose.rememberNavController
@@ -20,77 +23,123 @@ import com.example.boardgamestats.api.queryXmlApi
 import com.example.boardgamestats.ui.components.BottomNavigationBar
 import com.example.boardgamestats.ui.components.BottomNavigationGraph
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     var text by rememberSaveable { mutableStateOf("") }
     var active by rememberSaveable { mutableStateOf(false) }
+    var showOpenedIcons by rememberSaveable { mutableStateOf(false) }
     var searchResults by rememberSaveable { mutableStateOf(emptyList<BoardGame>()) }
+    var isLoading by rememberSaveable { mutableStateOf(false) }
 
     val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
+
+    var searchJob by rememberSaveable { mutableStateOf<Job?>(null) }
+    var wasSearched by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(active) {
+        delay(100)
+        showOpenedIcons = active
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Scaffold(bottomBar = {
         BottomNavigationBar(navController)
-    },
-        snackbarHost = { SnackbarHost(snackbarHostState) },
-        topBar = {
-            Box(Modifier.fillMaxWidth().zIndex(3f).padding(bottom = 8.dp)) {
-                SearchBar(
-                    modifier = Modifier.align(Alignment.TopCenter).padding(0.dp),
-                    query = text,
-                    onQueryChange = { text = it },
-                    onSearch = {
-                        GlobalScope.launch {
-                            searchResults =
-                                queryXmlApi("https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=$text")
+    }, snackbarHost = { SnackbarHost(snackbarHostState) }, topBar = {
+        Box(Modifier.fillMaxWidth().zIndex(3f).padding(bottom = 8.dp)) {
+            SearchBar(modifier = Modifier.align(Alignment.TopCenter).padding(0.dp),
+                query = text,
+                onQueryChange = { text = it },
+                onSearch = {
+                    keyboardController?.hide()
+                    wasSearched = true
+                    isLoading = true
+                    searchJob?.cancel()
+                    searchJob = GlobalScope.launch {
+                        searchResults =
+                            queryXmlApi("https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=$text")
+                        isLoading = false
+                    }
+                },
+                active = active,
+                onActiveChange = {
+                    active = it
+                    if (!it) {
+                        searchResults = emptyList()
+                        searchJob?.cancel()
+                        isLoading = false
+                        searchJob = null
+                        wasSearched = false
+                        text = ""
+                    }
+                },
+                placeholder = { Text("Search games") },
+                leadingIcon = {
+                    if (showOpenedIcons) {
+                        IconButton(
+                            onClick = { active = false }) {
+                            Icon(
+                                Icons.Default.ArrowBack,
+                                contentDescription = "Back"
+                            )
                         }
-                    },
-                    active = active,
-                    onActiveChange = {
-                        active = it
-                        if (!it) {
-                            searchResults = emptyList()
-                        }
-                    },
-                    placeholder = { Text("Hinted search text") },
-                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-                    trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
-                ) {
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        if (searchResults.isNotEmpty()) {
-                            LazyColumn(
-                                modifier = Modifier.fillMaxWidth(),
-                                contentPadding = PaddingValues(16.dp),
-                                verticalArrangement = Arrangement.spacedBy(4.dp)
-                            ) {
-                                items(searchResults.size) { idx ->
-                                    val boardGame = searchResults[idx]
-                                    ListItem(
-                                        headlineContent = { Text(boardGame.name) },
-                                        supportingContent = { Text(boardGame.publishYear.toString()) },
-//                                        leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
-//                                        modifier = Modifier.clickable {
-//                                            text = resultText
-//                                            active = false
-//                                        }
-                                    )
-                                }
+                    } else {
+                        Icon(Icons.Default.Search, contentDescription = "Search")
+                    }
+                },
+                trailingIcon = {
+                    Crossfade(targetState = showOpenedIcons && text.isNotEmpty()) {
+                        if (it) {
+                            IconButton(onClick = { text = "" }) {
+                                Icon(
+                                    Icons.Default.Clear,
+                                    contentDescription = "Clear"
+                                )
                             }
-                        } else {
-                            Text("No results found")
                         }
+                    }
+
+                }
+                ) {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    if (isLoading) {
+                        CircularProgressIndicator(Modifier.align(Alignment.Center))
+                    } else if (searchResults.isNotEmpty()) {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentPadding = PaddingValues(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ) {
+                            items(searchResults.size) { idx ->
+                                val boardGame = searchResults[idx]
+                                ListItem(
+                                    headlineContent = { Text(boardGame.name) },
+                                    supportingContent = { Text(boardGame.publishYear.toString()) },
+                                )
+                            }
+                        }
+                    } else if (wasSearched) {
+                        Text("No results found", modifier = Modifier.align(Alignment.Center))
                     }
                 }
             }
-        }) { padding ->
+        }
+    }) { padding ->
         Box(Modifier.padding(padding).fillMaxSize()) {
             BottomNavigationGraph(navController)
         }
     }
 
+    DisposableEffect(key1 = searchJob) {
+        onDispose {
+            searchJob?.cancel()
+        }
+    }
 }
